5eb8421ad26481738259af218aaecf5b203b9906
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-06 19:40:06 +0530

    diffrence scanner class type

--- input.java
+++ input.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int sum = a + b;
+//        System.out.println(sum);
+//        String name = sc.next();
+        String name = sc.nextLine();
+        System.out.println(name);
     }
 }
