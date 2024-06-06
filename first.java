098215b72613592a162f88b42c0b7e8746737e48
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-06 19:35:55 +0530

    Input Method

--- first.java
+++ first.java
@@ -1,5 +1,7 @@
 public class first {
     public static void main(String[] args) {
+//        System.out.println("Hello"); tis line is usr to print otput function
         System.out.println("Hello");
+
     }
 }
