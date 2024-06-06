8c18e5d113536ebffd50d934edf8abfc13c4cf07
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-06 21:36:25 +0530

    example of if odd or even

--- ifCondition.java
+++ ifCondition.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class ifCondition {
     public static void main(String[] args) {
+//        if age>=18 than print adult or not adult
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Age : ");
-        int age = sc.nextInt();
-        if (age >= 18){
-            System.out.println("You are Adult ");
+//        System.out.println("Enter Age : ");
+//        int age = sc.nextInt();
+//        if (age >= 18){
+//            System.out.println("You are Adult ");
+//        }else {
+//            System.out.println("You are Not Adult");
+//        }
+
+//        print odd or even number using java
+        System.out.print("Enter Number : ");
+        int x = sc.nextInt();
+        if( x % 2 == 0 ){
+            System.out.println("Even");
         }else {
-            System.out.println("You are Not Adult");
+            System.out.println("Odd");
         }
     }
+
 }
