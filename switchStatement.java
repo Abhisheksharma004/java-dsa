94453b43e5894b7fd09033210f1e5af4dc786543
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-07 09:42:49 +0530

    switch example

---
+++ switchStatement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class switchStatement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a){
+            case 1 :
+                System.out.println("Hello");
+                break;
+            case 2 :
+                System.out.println("Namste");
+                break;
+            case 3 :
+                System.out.println("Banjour");
+                break;
+            default:
+                System.out.println("Invalid Case");
+        }
+    }
+}
