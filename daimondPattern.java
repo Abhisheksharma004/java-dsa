ee383a5f64bddcc03a37d408fbcec792efc26382
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-06-14 12:09:10 +0530

    daimond pattern complete

--- daimondPattern.java
+++ daimondPattern.java
@@ -1,8 +1,6 @@
 public class daimondPattern {
     public static void main(String[] args) {
-        int n = 7; // Height of the diamond pattern
-
-        // Top half of the diamond (including the middle row)
+        int n = 7;
         for (int i = 0; i < n / 2 + 1; i++) {
             for (int j = 0; j < n; j++) {
                 if (j >= (n / 2 - i) && j <= (n / 2 + i)) {
@@ -13,8 +11,6 @@ public class daimondPattern {
             }
             System.out.println();
         }
-
-        // Bottom half of the diamond
         for (int i = n / 2 - 1; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
                 if (j >= (n / 2 - i) && j <= (n / 2 + i)) {
