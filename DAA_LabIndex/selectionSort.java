be5873e875b9ac25d07148704246119df4b2010e
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-08-14 11:07:22 +0530

    Daa Lab Index 1

---
+++ DAA_LabIndex/selectionSort.java
@@ -0,0 +1,30 @@
+public class selectionSort {
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the unsorted array
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+        selectionSort(array);
+        System.out.println("Sorted array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
