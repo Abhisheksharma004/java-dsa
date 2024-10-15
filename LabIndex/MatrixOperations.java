7f3fe5402a2306eea6d0db53026ca90d41ab6528
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-10-16 00:17:51 +0530

    lab 12

---
+++ LabIndex/MatrixOperations.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] sumMatrix = new int[rows][cols];
+        int[][] productMatrix = new int[rows][cols];
+
+        System.out.println("Enter elements of matrix 1:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of matrix 2:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                productMatrix[i][j] = matrix1[i][j] * matrix2[i][j];
+            }
+        }
+
+        System.out.println("Sum of matrices:");
+        printMatrix(sumMatrix);
+
+        System.out.println("Element-wise product of matrices:");
+        printMatrix(productMatrix);
+
+        sc.close();
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
